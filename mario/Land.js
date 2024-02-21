762f21d47596c03871ba57d2d19079bbc747691a
Ted Martinez <tedma4@gmail.com>
2024-02-20 20:50:01 -0700

    fix: land issue

--- mario/Land.js
+++ mario/Land.js
@@ -1,112 +1,74 @@
-scene('game', () => {
-  const map = [
-    '                                                                                                                                        ',
-    '                                                                                            =============                               ',
-    '                                                                               %          =                        %                    ',
-    '                                                                                        =                   =                           ',
-    '                                                              ^            =       ^   =                    =                           ',
-    '                                                           ============    ============                      ===========                ',
-    '    %     =*===                                           =                                                              =              ',
-    '                           -+                            =                                                                 =============',
-    '                 ^    ^    ()                           =                                                                               ',
-    '================================   ======   ============                                                                                '
-  ];
+const map = [
+  '                                                                                                                                        ',
+  '                                                                                            =============                               ',
+  '                                                                                %          =                        %                    ',
+  '                                                                                        =                   =                           ',
+  '                                                              ^            =       ^   =                    =                           ',
+  '                                                           ============    ============                      ===========                ',
+  '    %     =*===                                           =                                                              =              ',
+  '                           -+                            =                                                                 =============',
+  '                 ^    ^    ()                           =                                                                               ',
+  '================================   ======   ============                                                                                '
+];
 
-  const levelCfg = {
-    width: 20,
-    height: 20,
-    tileWidth: 20,
-    tileHeight: 20,
-    tiles: {
-      '=': () => [sprite('brick'), area(), body({ isStatic: true })],
-      $: () => [sprite('coin')],
-      '%': () => [
-        sprite('surprise'),
-        'coin-surprise',
-        area(),
-        body({ isStatic: true })
-      ],
-      '*': () => [
-        sprite('surprise'),
-        'mushroom-surprise',
-        area(),
-        body({ isStatic: true })
-      ],
-      '{': () => [sprite('unboxed')],
-      '(': () => [sprite('pipe-bottom-left'), scale(0.5)],
-      ')': () => [sprite('pipe-bottom-right'), scale(0.5)],
-      '-': () => [sprite('pipe-top-left'), scale(0.5)],
-      '+': () => [sprite('pipe-top-right'), scale(0.5)],
-      '^': () => [
-        sprite('evil-shroom'),
-        'dangerous',
-        area(),
-        body(),
-        move(LEFT, 35)
-      ]
-    }
-  };
+const levelCfg = {
+  width: 20,
+  height: 20,
+  tileWidth: 20,
+  tileHeight: 20,
+  tiles: {
+    '=': () => [sprite('brick'), area(), body({ isStatic: true })],
+    $: () => [sprite('coin')],
+    '%': () => [
+      sprite('surprise'),
+      'coin-surprise',
+      area(),
+      body({ isStatic: true })
+    ],
+    '*': () => [
+      sprite('surprise'),
+      'mushroom-surprise',
+      area(),
+      body({ isStatic: true })
+    ],
+    '{': () => [sprite('unboxed')],
+    '(': () => [sprite('pipe-bottom-left'), scale(0.5)],
+    ')': () => [sprite('pipe-bottom-right'), scale(0.5)],
+    '-': () => [sprite('pipe-top-left'), scale(0.5)],
+    '+': () => [sprite('pipe-top-right'), scale(0.5)],
+    '^': () => [
+      sprite('evil-shroom'),
+      'dangerous',
+      area(),
+      body(),
+      move(LEFT, 35)
+    ]
+  }
+};
+function createGameLevel() {
+  return addLevel(map, levelCfg);
+}
 
-  const gameLevel = addLevel(map, levelCfg);
-  const scoreLabel = add([
+function createScoreLabel() {
+  return add([
     text('score'),
     pos(30, 6),
     {
       value: 0
     }
   ]);
-  // add([text('level' + 'test', pos(4, 6))]);
+}
 
-  function big() {
-    let timer = 0;
-    let isBig = false;
-    return {
-      update() {
-        if (isBig) {
-          timer -= dt();
-          if (timer <= 0) {
-            this.smallify();
-          }
-        }
-        // runs 60 times per second
-        if (player.pos.y >= height()) {
-          // end the game or switch to a different scene
-          go('game'); // assuming 'gameover' is a scene you've defined
-        }
-        if (player.isGrounded()) {
-          isJumping = false;
-        }
-        camPos(player.pos);
-        if (player.pos.y >= FALL_DEATH) {
-          go('game', { score: scoreLabel.value });
-        }
-      },
-      isBig() {
-        return isBig;
-      },
-      smallify() {
-        player.currentJumpForce = player.jumpForce;
-        this.scale = vec2(1);
-        timer = 0;
-        isBig = false;
-      },
-      biggify(time) {
-        player.currentJumpForce = player.bigJumpForce;
-        this.scale = vec2(2);
-        timer = time;
-        isBig = true;
-      }
-    };
-  }
+const FALL_DEATH = 400;
+let scoreLabel;
+let isJumping = true;
 
-  const player = add([sprite('mario'), pos(30, 0), body(), area(), big()]);
+function playerStuff(player, scoreLabel) {
   // Set the player's move speed and jump force
   player.moveSpeed = 200;
   player.jumpForce = 600;
   player.bigJumpForce = 1000;
   player.currentJumpForce = player.jumpForce;
-  let isJumping = true;
-  const FALL_DEATH = 400;
 
   player.onCollide('coin-surprise', (obj) => {
     const gridSize = 20; // The height of a grid cell
@@ -166,10 +128,55 @@ scene('game', () => {
       player.jump(player.currentJumpForce);
     }
   });
-  // check if player falls off the map
-  // loop(1 / 60, () => {
+}
 
-  // });
+let player;
+function big() {
+  let timer = 0;
+  let isBig = false;
+  return {
+    update() {
+      if (isBig) {
+        timer -= dt();
+        if (timer <= 0) {
+          this.smallify();
+        }
+      }
+      // runs 60 times per second
+      if (player.pos.y >= height()) {
+        // end the game or switch to a different scene
+        go('game'); // assuming 'gameover' is a scene you've defined
+      }
+      if (player.isGrounded()) {
+        isJumping = false;
+      }
+      camPos(player.pos);
+      if (player.pos.y >= FALL_DEATH) {
+        go('game', { score: scoreLabel.value });
+      }
+    },
+    isBig() {
+      return isBig;
+    },
+    smallify() {
+      player.currentJumpForce = player.jumpForce;
+      this.scale = vec2(1);
+      timer = 0;
+      isBig = false;
+    },
+    biggify(time) {
+      player.currentJumpForce = player.bigJumpForce;
+      this.scale = vec2(2);
+      timer = time;
+      isBig = true;
+    }
+  };
+}
+scene('game', () => {
+  const gameLevel = createGameLevel();
+  scoreLabel = createScoreLabel();
+  player = add([sprite('mario'), pos(30, 0), body(), area(), big()]);
+  playerStuff(player, scoreLabel);
 });
 
 go('game');
