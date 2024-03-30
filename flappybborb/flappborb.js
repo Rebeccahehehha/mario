6a933ca6e50e0938c59425804eec8feabb238033
Rebecca <rebeccamyhero2010@gmail.com>
2024-03-29 17:07:33 -0700

    Tubes moving

--- flappybborb/flappborb.js
+++ flappybborb/flappborb.js
@@ -4,30 +4,56 @@ loadSprite('Background', 'PixelBackground.png');
 loadSprite('Poptartcat', 'Poptartcat.png');
 loadSprite('Tube', 'Tube.png');
 
-add([sprite('Background')]);
-setGravity(2000);
-const player = add([
-  sprite('Poptartcat'),
-  scale(0.5),
-  body(),
-  area(),
-  pos(80, 40)
-]);
-player.currentJumpForce = 600;
-onKeyPress('space', () => {
-  // isJumping = true;
-  player.jump(player.currentJumpForce);
+scene('game', () => {
+  add([sprite('Background', { width: width(), height: height() })]);
+  setGravity(2000);
+  const player = add([
+    sprite('Poptartcat'),
+    scale(0.5),
+    body(),
+    area(),
+    pos(80, 40)
+  ]);
+  player.currentJumpForce = 600;
+  onKeyPress('space', () => {
+    // isJumping = true;
+    player.jump(player.currentJumpForce);
+  });
+  player.onCollide('Tube', () => {
+    go('Gameover');
+  });
+  player.onUpdate(() => {
+    if (player.pos.y > height() || player.pos.y < 0) {
+      go('Gameover');
+    }
+  });
+
+  const tubeGap = 200;
+  const offset = rand(-50, 50);
+  add([
+    sprite('Tube'),
+    scale(0.5),
+    pos(width() - 100, height() / 2 + offset + tubeGap / 2),
+    'Tube',
+    area()
+  ]);
+  add([
+    sprite('Tube', { flipY: true }),
+    scale(0.5),
+    pos(width() - 100, height() / 2 + offset - tubeGap / 2),
+    anchor('botleft'),
+    'Tube',
+    area()
+  ]);
+  onUpdate('Tube', (Tube) => {
+    Tube.move(-500, 0);
+  });
+});
+scene('Gameover', () => {
+  add([text('Gameover:D')]);
+  onKeyPress('space', () => {
+    go('game');
+  });
 });
 
-const tubeGap = 800;
-add([
-  sprite('Tube'),
-  scale(0.5),
-  pos(width() - 100, height() / 2 + tubeGap / 2)
-]);
-add([
-  sprite('Tube', { flipY: true }),
-  scale(0.5),
-  pos(width() - 100, height() / 2 - tubeGap / 2),
-  origin('botleft')
-]);
+go('game');
